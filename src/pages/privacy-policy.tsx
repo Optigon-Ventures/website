e8ed35791b3c49e124920c8c44c12b0e7d076f1c
iamnoufal <jnrahman12@gmail.com>
2023-09-17 00:07:46 +0530

    commit

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/Layout";
 
-export default function PrivacyPolicyPage({ data } : any) {
-  console.log(data)
+export default function PrivacyPolicyPage({ data }: any) {
+  console.log(data);
   return (
     <Layout>
       <div className="display-2 text-center fw-bold">Privacy Policy</div>
@@ -15,10 +15,11 @@ export async function getServerSideProps() {
     `${process.env.STRAPI_API_URI}/api/website?fields[0]=privacyPolicy`,
     { headers: { Authorization: `Bearer ${process.env.STRAPI_API_KEY}` } }
   );
-  const respData = await resp.json()
-  let data = respData.data.attributes.privacyPolicy
-  console.log(data)
-  return { props: {
-    data: data
-  } }
+  const respData = await resp.json();
+  let data = respData.data.attributes.privacyPolicy;
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
--- src/pages/terms-and-conditions.tsx
+++ src/pages/terms-and-conditions.tsx
@@ -1,9 +1,24 @@
 import Layout from "@/components/Layout";
 
-export default function TermsAndConditionsPage() {
+export default function TermsAndConditionsPage({ data }: any) {
   return (
     <Layout>
       <div className="display-2 text-center fw-bold">Terms and Conditions</div>
+      {data}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const resp = await fetch(
+    `${process.env.STRAPI_API_URI}/api/website?fields[0]=termsAndConditions`,
+    { headers: { Authorization: `Bearer ${process.env.STRAPI_API_KEY}` } }
+  );
+  const respData = await resp.json();
+  let data = respData.data.attributes.termsAndConditions;
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
