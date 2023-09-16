c8b3a9fd329b069a779e2f4cdaf1c1cb645eeacf
iamnoufal <jnrahman12@gmail.com>
2023-09-17 00:07:26 +0530

    strapi cms

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,9 +1,24 @@
 import Layout from "@/components/Layout";
 
-export default function PrivacyPolicyPage() {
+export default function PrivacyPolicyPage({ data } : any) {
+  console.log(data)
   return (
     <Layout>
       <div className="display-2 text-center fw-bold">Privacy Policy</div>
+      {data}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const resp = await fetch(
+    `${process.env.STRAPI_API_URI}/api/website?fields[0]=privacyPolicy`,
+    { headers: { Authorization: `Bearer ${process.env.STRAPI_API_KEY}` } }
+  );
+  const respData = await resp.json()
+  let data = respData.data.attributes.privacyPolicy
+  console.log(data)
+  return { props: {
+    data: data
+  } }
 }
