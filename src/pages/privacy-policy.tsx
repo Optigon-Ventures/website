6478d6505f3d66a0d28c77d55ba72a4e23b25595
iamnoufal <jnrahman12@gmail.com>
2023-09-17 00:58:02 +0530

    terms and conditions and privacy policy linked to strapi

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,22 +1,27 @@
 import Layout from "@/components/Layout";
 
-export default function PrivacyPolicyPage({ data }: any) {
-  console.log(data);
+export default function PrivacyPolicyPage({ data } : any) {
   return (
     <Layout>
-      <div className="display-2 text-center fw-bold">Privacy Policy</div>
-      {data}
+      <div className="display-6 mt-5 mb-4 text-center fw-bold">Privacy Policy</div>
+      <div className="container">
+        <ul>
+          {data.map((item: any) => <li>{item}</li>)}
+        </ul>
+      </div>
     </Layout>
   );
 }
 
 export async function getServerSideProps() {
   const resp = await fetch(
-    `${process.env.STRAPI_API_URI}/api/website?fields[0]=privacyPolicy`,
+    `${process.env.STRAPI_API_URI}/api/website?populate[privacyPolicy]=*`,
     { headers: { Authorization: `Bearer ${process.env.STRAPI_API_KEY}` } }
   );
   const respData = await resp.json();
-  let data = respData.data.attributes.privacyPolicy;
+  let data = respData.data.attributes.privacyPolicy.map((item: any) => item.item);
+
+  console.log(data)
   return {
     props: {
       data: data,
--- src/pages/terms-and-conditions.tsx
+++ src/pages/terms-and-conditions.tsx
@@ -3,19 +3,23 @@ import Layout from "@/components/Layout";
 export default function TermsAndConditionsPage({ data }: any) {
   return (
     <Layout>
-      <div className="display-2 text-center fw-bold">Terms and Conditions</div>
-      {data}
+      <div className="display-6 mt-5 mb-4 text-center fw-bold">Terms and Conditions</div>
+      <div className="container">
+        <ul>
+          {data.map((item: any) => <li>{item}</li>)}
+        </ul>
+      </div>
     </Layout>
   );
 }
 
 export async function getServerSideProps() {
   const resp = await fetch(
-    `${process.env.STRAPI_API_URI}/api/website?fields[0]=termsAndConditions`,
+    `${process.env.STRAPI_API_URI}/api/website?populate[termsAndConditions]=*`,
     { headers: { Authorization: `Bearer ${process.env.STRAPI_API_KEY}` } }
   );
   const respData = await resp.json();
-  let data = respData.data.attributes.termsAndConditions;
+  let data = respData.data.attributes.termsAndConditions.map((item: any) => item.item);
   return {
     props: {
       data: data,
